07a599d93048376017e38424ac16aee2df578492
alerenz <alexa111202@mail.ru>
2025-05-03 22:05:42 +0400

    Добавление swagger

--- app/Http/Controllers/Api/PromocodeController.php
+++ app/Http/Controllers/Api/PromocodeController.php
@@ -12,16 +12,57 @@ use App\Models\UserPrize;
 class PromocodeController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * 
+     * @OA\Get(
+     *    path="/api/promocode",
+     *    summary="Получение списка промокодов",
+     *    tags={"Промокоды"},
+     *    security={{"bearerAuth":{"role": "admin"} }},
+     *
+     *    @OA\Response(
+     *        response=200,
+     *        description="ОК",
+     *      
+     *    ),
+     * )
      */
     public function index()
     {
         return Promocode::all();
     }
 
-
     /**
-     * Store a newly created resource in storage.
+     * 
+     * @OA\Post(
+     *    path="/api/promocode",
+     *    summary="Создание промокода",
+     *    tags={"Промокоды"},
+     *    security={{"bearerAuth":{} }},
+     * 
+     * 
+     *    @OA\RequestBody(
+     *        @OA\JsonContent(
+     *            allOf={
+     *                @OA\Schema(
+     *                    @OA\Property(property="type_discount", type="enum", example="Процентная"),
+     *                    @OA\Property(property="discount_value", type="float", example=15),
+     *                    @OA\Property(property="expiry_date", type="date", example="2025-05-01"),
+     *                )
+     *            }
+     *        )
+     *    ),
+     * 
+     *    @OA\Response(
+     *        response=201,
+     *        description="ОК",
+     *        @OA\JsonContent(
+     *            @OA\Property(property="name", type="string", example="abc123"),
+     *            @OA\Property(property="type_discount", type="enum", example="Процентная"),
+     *            @OA\Property(property="discount_value", type="float", example=15),
+     *            @OA\Property(property="expiry_date", type="date", example="2025-05-01"),
+     *        )
+     *    ),
+     * )
      */
     public function store(StorePromocodeRequest $request)
     {
@@ -36,7 +77,27 @@ class PromocodeController extends Controller
 
 
     /**
-     * Display the specified resource.
+     * 
+     * @OA\Get(
+     *    path="/api/promocode/{promocode}",
+     *    summary="Получение промокода по id",
+     *    tags={"Промокоды"},
+     *    security={{"bearerAuth":{} }},
+     * 
+     *    @OA\Parameter(
+     *        description="id промокода",
+     *        in="path",
+     *        name="promocode",
+     *        required=true,
+     *        example=1
+     *    ),
+     *
+     *    @OA\Response(
+     *        response=200,
+     *        description="ОК",
+     *      
+     *    ),
+     * )
      */
     public function show(Promocode $promocode)
     {
@@ -46,7 +107,45 @@ class PromocodeController extends Controller
 
 
     /**
-     * Update the specified resource in storage.
+     * 
+     * @OA\Put(
+     *    path="/api/promocode/{promocode}",
+     *    summary="Обновление промокода  по id",
+     *    tags={"Промокоды"},
+     *    security={{"bearerAuth":{} }},
+     * 
+     *    @OA\Parameter(
+     *        description="id промокода",
+     *        in="path",
+     *        name="promocode",
+     *        required=true,
+     *        example=1
+     *    ),
+     * 
+     * 
+     *    @OA\RequestBody(
+     *        @OA\JsonContent(
+     *            allOf={
+     *                @OA\Schema(
+     *                    @OA\Property(property="type_discount", type="enum", example="Процентная"),
+     *                    @OA\Property(property="discount_value", type="float", example=15),
+     *                    @OA\Property(property="expiry_date", type="date", example="2025-05-01"),
+     *                )
+     *            }
+     *        )
+     *    ),
+     * 
+     *    @OA\Response(
+     *        response=200,
+     *        description="ОК",
+     *        @OA\JsonContent(
+     *            @OA\Property(property="name", type="string", example="abc123"),
+     *            @OA\Property(property="type_discount", type="enum", example="Процентная"),
+     *            @OA\Property(property="discount_value", type="float", example=15),
+     *            @OA\Property(property="expiry_date", type="date", example="2025-05-01"),
+     *        )
+     *    ),
+     * )
      */
     public function update(UpdatePromocodeRequest $request, $id)
     {
@@ -61,7 +160,31 @@ class PromocodeController extends Controller
 
 
     /**
-     * Remove the specified resource from storage.
+     * 
+     * @OA\Delete(
+     *    path="/api/promocode/{promocode}",
+     *    summary="Удаление промокода по id",
+     *    tags={"Промокоды"},
+     *    security={{"bearerAuth":{} }},
+     * 
+     *    @OA\Parameter(
+     *        description="id промокода",
+     *        in="path",
+     *        name="promocode",
+     *        required=true,
+     *        example=1
+     *    ),
+     *
+     *    @OA\Response(
+     *        response=200,
+     *        description="Промокод успешно удален",  
+     *    ),
+     * 
+     *    @OA\Response(
+     *        response=403,
+     *        description="Этот приз удалить нельзя, его выйграли",  
+     *    ),
+     * )
      */
     public function destroy($id)
     {
