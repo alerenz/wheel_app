50e05fc15fe246d63f592439c2c5f4ddff10bf9a
alerenz <alexa111202@mail.ru>
2025-05-04 13:56:48 +0400

    Добавила аннотации swagger

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,7 +9,37 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-
+    /**
+     * 
+     * @OA\Post(
+     *    path="/api/auth/register",
+     *    summary="Регистрация пользователя",
+     *    tags={"Auth"},
+     *    
+     *    @OA\RequestBody(
+     *        @OA\JsonContent(
+     *            allOf={
+     *                @OA\Schema(
+     *                    @OA\Property(property="username", type="string", example="user123"),
+     *                    @OA\Property(property="password", type="string", example="qwerty12345"),
+     *                    @OA\Property(property="surname", type="string", example="Иванов"),
+     *                    @OA\Property(property="name", type="string", example="Иван"),
+     *                    @OA\Property(property="patronymic", type="string", example="Иванович"),
+     *                )
+     *            }
+     *        )
+     *    ),
+     * 
+     *    @OA\Response(
+     *        response=201,
+     *        description="Пользователь успешно зарегистрирован",
+     *        @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Пользователь успешно зарегистрирован")
+     *        )
+     *        
+     *    ),
+     * )
+     */
     
 
     public function register(Request $request)
@@ -28,15 +58,41 @@ class AuthController extends Controller
             'surname'=>$request->surname,
             'name'=>$request->name,
             'patronymic'=>$request->patronymic,
-            'role'=>'admin',
+            'role'=>'user',
             'active'=>true,
             
             
         ]);
 
-        return response()->json(['message' => 'User registered successfully'], 201);
+        return response()->json(['message' => 'Пользователь успешно зарегистрирован'], 201);
     }
 
+    /**
+     * 
+     * @OA\Post(
+     *    path="/api/auth/login",
+     *    summary="Авторизация пользователя",
+     *    tags={"Auth"},
+     *    
+     *    @OA\RequestBody(
+     *        @OA\JsonContent(
+     *            allOf={
+     *                @OA\Schema(
+     *                    @OA\Property(property="username", type="string", example="user123"),
+     *                    @OA\Property(property="password", type="string", example="qwerty12345"),
+     *                    
+     *                )
+     *            }
+     *        )
+     *    ),
+     * 
+     *    @OA\Response(
+     *        response=200,
+     *        description="ОК",
+     *        
+     *    ),
+     * )
+     */
 
     public function login(){
         $credentials = request(['username', 'password']);
@@ -48,14 +104,77 @@ class AuthController extends Controller
         return $this->respondWithToken($token);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/auth/show",
+     *     summary="Получить информацию о текущем пользователе",
+     *     security={{"bearerAuth": {} }},
+     *     tags={"Auth"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Успешное получение данных пользователя",
+     *        
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Неавторизованный доступ",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Unauthenticated.")
+     *         )
+     *     )
+     * )
+     */
+
     public function user(){
         return response()->json(auth('api')->user());
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/auth/logout",
+     *     summary="Выход пользователя",
+     *     security={{"bearerAuth": {} }},
+     *     tags={"Auth"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Выход произошел успешно",
+     *        
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Неавторизованный доступ",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Unauthenticated.")
+     *         )
+     *     )
+     * )
+     */
+
     public function logout(){
         auth('api')->logout();
-        return response()->json(['message' => 'Successfully logged out']);
+        return response()->json(['message' => 'Выход произошел успешно']);
     }
+
+    /**
+     * @OA\Post(
+     *     path="/api/auth/refresh",
+     *     summary="Обновление токена",
+     *     security={{"bearerAuth": {} }},
+     *     tags={"Auth"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *        
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Неавторизованный доступ",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Unauthenticated.")
+     *         )
+     *     )
+     * )
+     */
     
     public function refresh(){
         return $this->respondWithToken(auth('api')->refresh());
