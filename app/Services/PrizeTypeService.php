f10ce2ae54065f3930629df0ada53998ea3a0986
alerenz <alexa111202@mail.ru>
2025-05-07 12:11:16 +0400

    Добавила класс для преобразования класса в строку и наоборот

---
+++ app/Services/PrizeTypeService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+use InvalidArgumentException;
+use App\Models\Promocode;
+use App\Models\EmptyPrize;
+use App\Models\MaterialThing;
+
+
+class PrizeTypeService
+{
+    protected static $prizeTypes = [
+        'promocode' => Promocode::class,
+        'material_thing' => MaterialThing::class,
+        'empty_prize' => EmptyPrize::class,
+    ];
+
+    /**
+     * Преобразует класс приза в строку.
+     *
+     * @param string $class
+     * @return string|null
+     */
+    public static function classToString(string $class): ?string
+    {
+        return array_search($class, self::$prizeTypes) ?: null;
+    }
+
+    /**
+     * Преобразует строку приза в класс.
+     *
+     * @param string $type
+     * @return string
+     * @throws InvalidArgumentException
+     */
+    public static function stringToClass(string $type): string
+    {
+        if (!isset(self::$prizeTypes[$type])) {
+            throw new InvalidArgumentException('Некорректный тип приза');
+        }
+        return self::$prizeTypes[$type];
+    }
+}
