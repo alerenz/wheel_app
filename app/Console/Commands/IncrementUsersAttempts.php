f53303014856c9196feca9542f8556286bdc2a54
alerenz <alexa111202@mail.ru>
2025-05-12 15:37:13 +0400

    Добавила транзакцию для выполнения команды

--- app/Console/Commands/IncrementUsersAttempts.php
+++ app/Console/Commands/IncrementUsersAttempts.php
@@ -6,6 +6,9 @@ use Illuminate\Console\Command;
 use App\Models\User;
 use App\Models\Wheel;
 use App\Enums\StatusWheelType;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class IncrementUsersAttempts extends Command
 {
@@ -28,29 +31,38 @@ class IncrementUsersAttempts extends Command
      */
     public function handle()
     {
-        $currentDayIndex = date('w');
-        
-        $daysMap = [
-            0 => 'воскресенье',
-            1 => 'понедельник',
-            2 => 'вторник',
-            3 => 'среда',
-            4 => 'четверг',
-            5 => 'пятница',
-            6 => 'суббота'
-        ];
-    
-        $currentDay = $daysMap[$currentDayIndex];
-
-        $wheels = Wheel::where('status', StatusWheelType::active->value)->get();
-        foreach($wheels as $wheel){
-            $wheelDays = json_decode($wheel->days_of_week);
-            if (in_array($currentDay, $wheelDays)) {
-                User::where('attempts', '<', 5)
-                        ->increment('attempts');
+        DB::transaction(function () {
+            try {
+                $currentDayIndex = date('w');
+                $max_attempts = config('custom.max_attempts');
                 
-                $this->info("Попытки для прокрутки колеса {$wheel->name} увеличились для всех пользователей");
+                $daysMap = [
+                    0 => 'воскресенье',
+                    1 => 'понедельник',
+                    2 => 'вторник',
+                    3 => 'среда',
+                    4 => 'четверг',
+                    5 => 'пятница',
+                    6 => 'суббота'
+                ];
+            
+                $currentDay = $daysMap[$currentDayIndex];
+
+                $wheels = Wheel::where('status', StatusWheelType::active->value)->get();
+                foreach($wheels as $wheel){
+                    $wheelDays = json_decode($wheel->days_of_week);
+                    if (in_array($currentDay, $wheelDays)) {
+                        User::where('attempts', '<', $max_attempts)
+                                ->increment('attempts');
+                        
+                        Log::info("Попытки для прокрутки колеса {$wheel->name} увеличились для всех пользователей");
+                    }
+                }
+            }catch(\Exception $e){
+                Log::error("Ошибка при инкременте попыток: " . $e->getMessage());
+                throw $e;
             }
-        }
+        });
+        
     }
 }
