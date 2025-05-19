8d8525d6dc68b9cbf571ebcce0f348956e9270b2
alerenz <alexa111202@mail.ru>
2025-05-19 11:04:20 +0400

    Исправила ошибку с доступом к двум функциям

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,8 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('show', [AuthController::class, 'user']);
 });
 
 
@@ -72,8 +74,7 @@ Route::group(['middleware'=>['jwt.auth', 'role:admin']], function ($router) {
     Route::put('attempt/{id}',[AttemptController::class,'update']);
     Route::delete('attempt/{id}',[AttemptController::class,'destroy']);
 
-    Route::post('auth/logout', [AuthController::class, 'logout']);
-    Route::get('auth/show', [AuthController::class, 'user']);
+
     
 });
 
